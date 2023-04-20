24f51e2c5fd5704fa63e27db35c945b78dc39622
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-04-21 00:27:21 +0530

    Data Service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Book } from "app/models/book";
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { LoggerService } from 'app/core/logger.service';
+import { DataService } from 'app/core/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,15 +13,20 @@ import { LoggerService } from 'app/core/logger.service';
 })
 export class DashboardComponent implements OnInit {
 
-  allBooks: Book[] = allBooks;
-  allReaders: Reader[] = allReaders;
-  mostPopularBook: Book = allBooks[0];
+  allBooks: Book[];
+  allReaders: Reader[];
+  mostPopularBook: Book;
 
 
-  constructor(private loggerService: LoggerService) {
+  constructor(private loggerService: LoggerService,
+    private dataService: DataService) {
+
   }
 
   ngOnInit() {
+    this.allBooks = this.dataService.getAllBooks();
+    this.allReaders = this.dataService.getAllReaders();
+    this.mostPopularBook = this.allBooks[0];
   }
 
   deleteBook(bookID: number): void {
